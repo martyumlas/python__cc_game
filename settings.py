d2f2b3577d261db977993336e9597e4c1dfa2b8c
martyumlas <martyumlas@gmail.com>
2024-03-18 19:03:59 +0800

    add settings class

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    '''a class to store all settings for the alien invasion'''
+
+    def __init__(self):
+        '''initialize game settings'''
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+    
