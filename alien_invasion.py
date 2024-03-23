da2f1b8cf78e544761f58beef21935acae28617e
martyumlas <martyumlas@gmail.com>
2024-03-23 15:48:47 +0800

    shooting aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -101,6 +101,15 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
             print(len(self.bullets))
+        
+        # check for any bullets that have hit aliens
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
 
     def _create_alien(self, x_position, y_position):
         new_alien = Alien(self)
